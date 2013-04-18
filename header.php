e3db2178956edf922a1d42fc549252fb3749386a
Jeremy Wilson <jeremy@gravitatedesign.com>
2013-04-18 09:16:11 -0700

    Updated header.php
    Removed IE conditionals and erroneous code surrounding it.  Commented
    out viewport meta tag.

--- header.php
+++ header.php
@@ -1,19 +1,12 @@
 <!doctype html>  
-<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?> class="no-js iem7"> <![endif]-->
-<!--[if lt IE 7]> <html class="ie6" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 9]>    <html class="ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="<?php language_attributes(); ?>><!--<![endif]-->
+<html <?php language_attributes(); ?>>
 
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		
 		<title><?php bloginfo("name"); ?> <?php wp_title("&bull;"); ?></title>
 		
-
-		<meta name="viewport" content="width=device-width">
+		<!--<meta name="viewport" content="width=device-width">-->
 		<meta name="application-name" content="<?php bloginfo('name'); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
@@ -30,10 +23,3 @@
 	</head>
 	
 	<body <?php body_class(); ?>>
-
-		
-		<!--
-		<nav role="navigation" class="nav">
-			<?php grav_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-		</nav>
-		-->
