2893dfa6478ee61bd9646aff38b2a3a0de3df838
Geoff Grav <geoff.gedde@gravitatedesign.com>
2013-12-23 15:15:51 -0800

    Added Body "id"  Updated formatting.

--- header.php
+++ header.php
@@ -1,24 +1,22 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
-	<head>
-	
-		<title><?php bloginfo("name"); ?> <?php wp_title('&bull;'); ?></title>
-		
-		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-		<meta name="viewport" content="width=device-width">
-		<meta name="application-name" content="<?php bloginfo('name'); ?>" />
-		
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		<?php wp_head(); ?>
-		<script type="text/javascript">
-			grav_theme = <?php echo '"' . get_template_directory_uri() . '"'; ?>;
-		</script>
-		<script data-main="<?php echo get_template_directory_uri(); ?>/library/js/require/require.config" src="<?php echo get_template_directory_uri(); ?>/library/js/require/require.min.js"></script>
-		
-	</head>
-	
+<head>
 
-	<body <?php body_class(); ?>>
-	
+<title><?php bloginfo("name"); ?> <?php wp_title('&bull;'); ?></title>
+
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width">
+<meta name="application-name" content="<?php bloginfo('name'); ?>" />
+
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+<?php wp_head(); ?>
+<script type="text/javascript">
+	grav_theme = <?php echo '"' . get_template_directory_uri() . '"'; ?>;
+</script>
+<script data-main="<?php echo get_template_directory_uri(); ?>/library/js/require/require.config" src="<?php echo get_template_directory_uri(); ?>/library/js/require/require.min.js"></script>
+
+</head>
+
+<body id="body" <?php body_class(); ?>>
