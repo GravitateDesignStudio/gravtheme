7e4369f478df9624834cbf9451a94b47d190b93c
bferdinand <brian@brianferdinand.com>
2014-05-08 08:35:18 -0700

    added script for opening a separate window.

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1,5 +1,20 @@
 (function($){
 
-	console.log('Hey No Love for AMD? /r/n Ok, I\'m sure there\'s probably a good reason you dont need to')
+	//console.log('Hey No Love for AMD? /r/n Ok, I\'m sure there\'s probably a good reason you dont need to')
+	
+	$('a[href$=".pdf"]').attr('target', '_blank');
+	hostname = new RegExp("//" + location.host + "($|/)");
+	$("a").each(function() {
+		var a = $(this).attr("href");
+		if(a != undefined){
+			if (hostname.test(a) || a.slice(0, 1) == "/") {} else {
+				if (a.slice(0, 1) == "#") {} else {
+					if ($(this).html() != 'Blog') {
+						$(this).attr('target', '_blank');
+					}
+				}
+			}
+		}
+	});
 
 })(jQuery)
