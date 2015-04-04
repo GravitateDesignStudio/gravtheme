2c6d9881da889dd9f96077b06a1af9b5ef8e43db
geoff-grav <geoff.gedde@gravitatedesign.com>
2015-04-03 17:09:11 -0700

    Updated notes

--- library/acf.php
+++ library/acf.php
@@ -1,4 +1,5 @@
 <?php
 
 define( 'ACF_LITE' , true ); // true hides acf from the admin panel. false shows it.
-//include_once( 'advanced-custom-fields/acf.php' );
+
+// Register your ACF Fields Below
