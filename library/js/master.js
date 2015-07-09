cd85a3fa8eafe66a89e79904dc4a03e036d30079
amc-gravitatedesign <aine.mccarthy@gravitatedesign.com>
2015-07-09 09:24:12 -0700

    Update master.js
    added equal column widths function

--- library/js/master.js
+++ library/js/master.js
@@ -11,7 +11,40 @@
 	});
 
 
-
+// equal column widths
+	function elEQ(e, f) {
+	    var estEl = 0, allEl = 0, initialEl = 0, el, curEl, elArray = [];
+	    $(e).each(function() {
+	    	var section = $(this).find(f);
+	    	section.each(function() {
+		        el = $(this);
+		        $(el).height('auto');
+		        initialEl = el.position().top;
+		        if (allEl != initialEl) {
+		            for (curEl = 0; curEl < elArray.length; curEl++) elArray[curEl].outerHeight(estEl);
+			            elArray.length = 0;
+			            allEl = initialEl;
+			            estEl = el.innerHeight();
+			            elArray.push(el);
+		        } else {
+		            elArray.push(el);
+		            estEl = estEl < el.innerHeight() ? el.innerHeight() : estEl;
+		        }
+		        for (curEl = 0; curEl < elArray.length; curEl++) elArray[curEl].innerHeight(estEl);
+		    });
+	    });
+	}
+	function eqHeights() {
+		// edit this area only
+		// call the elEQ function here for each leveling element, using parent element and child (leveling) element. 
+		// e.g.: elEQ('.block-grid', 'li');
+	}
+	$(window).load(function() {
+		eqHeights();
+	});
+	$(window).resize(function() {
+		eqHeights();
+	});
 
 
 
