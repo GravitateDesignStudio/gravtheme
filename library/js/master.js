94c5d9e7ffb307999e6083951864f71f48163a51
bferdinand <brian@brianferdinand.com>
2015-07-24 17:54:16 -0700

    updates

--- library/js/master.js
+++ library/js/master.js
@@ -1,17 +1,24 @@
+/*
+* Paste minified plugins
+*/
+
+
+/*
+* between comments
+*/
+
+
 (function($){
 
 
 
-	/* Make all External Links and PDF's open in a new Tab */
-    var host = new RegExp('/' + window.location.host + '/');
-    $('a').each(function() {
-	    if ((!host.test(this.href) && this.href.slice(0, 1) != "/" && this.href.slice(0, 1) != "#" && this.href.slice(0, 1) != "?") || this.href.indexOf('.pdf') > 0) {
-		    $(this).attr({'target': '_blank'});
-	    }
-	});
+	/*
+	*
+	* Functions
+	*
+	*/
 
-
-// equal column heights
+	// equal column heights
 	function elEQ(e, f) {
 	    var estEl = 0, allEl = 0, initialEl = 0, el, curEl, elArray = [];
 	    $(e).each(function() {
@@ -36,12 +43,47 @@
 	}
 	function eqHeights() {
 		// edit this area only
-		// call the elEQ function here for each leveling element, using parent element and child (leveling) element. 
+		// call the elEQ function here for each leveling element, using parent element and child (leveling) element.
 		// e.g.: elEQ('.block-grid', 'li');
 	}
+
+	/*
+	* End Functions
+	*/
+
+
+
+	/*
+	*
+	*	Place items in here to have them run after the Dom is ready
+	*
+	*/
+	$(document).ready(){
+		/* Make all External Links and PDF's open in a new Tab */
+	    var host = new RegExp('/' + window.location.host + '/');
+	    $('a').each(function() {
+		    if ((!host.test(this.href) && this.href.slice(0, 1) != "/" && this.href.slice(0, 1) != "#" && this.href.slice(0, 1) != "?") || this.href.indexOf('.pdf') > 0) {
+			    $(this).attr({'target': '_blank'});
+		    }
+		});
+
+
+	});
+
+	/*
+	*
+	*	Place items in here to have them run the page is loaded
+	*
+	*/
 	$(window).load(function() {
 		eqHeights();
 	});
+
+	/*
+	*
+	*	Place items in here to have them run when the window is resized
+	*
+	*/
 	$(window).resize(function() {
 		eqHeights();
 	});
