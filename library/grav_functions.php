b0d4a67a8e7eaaed7e3a4ecbe21e55c262bddfd9
bferdinand <brian@brianferdinand.com>
2015-10-20 10:29:19 -0700

    updated functions with walker

--- library/grav_functions.php
+++ library/grav_functions.php
@@ -1954,3 +1954,130 @@ function grav_privacy_settings(){
 		}
 	}
 }
+
+/**
+ * Creates a walker of child from WP menu
+ *
+ * @param  $output, depth, args  output of walker.
+ *
+ * @return walker
+ * @author BF
+ *
+ **/
+
+class grav_child_walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth=0, $args=array()) {
+		$indent = str_repeat("\t", $depth);
+		if($depth == 0){
+			//$output .= "\n$indent<div class=\"sub-menu\">\n";
+		} else {
+			$output .= "\n$indent<ul class=\"sub-menu row\">\n";
+		}
+	}
+
+	function end_lvl(&$output, $depth=0, $args=array()) {
+		$indent = str_repeat("\t", $depth);
+		if($depth == 0){
+			//$output .= "$indent</div>\n";
+		} else {
+			$output .= "$indent</ul>\n";
+		}
+	}
+
+	// Don't print top-level elements
+	function start_el(&$output, $item, $depth=0, $args=array()) {
+		if( $depth == 0 ){
+			$target = str_replace('#', '', $item->url);
+			$output .= '<ul id="'.$target.'" class="row hidden-nav">';
+			return;
+		} elseif( $depth == 1 ){
+			$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+			$classes[] = 'menu-item-' . $item->ID;
+
+			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+			$class_names = "medium-6 large-3 columns ".$class_names;
+			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+			$output .= $indent . '<li' . $id . $class_names .'>';
+
+			$atts = array();
+			$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+			$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+			$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+			$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+			$attributes = '';
+			foreach ( $atts as $attr => $value ) {
+				if ( ! empty( $value ) ) {
+					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+					$attributes .= ' ' . $attr . '="' . $value . '"';
+				}
+			}
+
+			$item_output = $args->before;
+			$item_output .= '<a'. $attributes .'>';
+			/** This filter is documented in wp-includes/post-template.php */
+			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+			$item_output .= '</a>';
+			$item_output .= $args->after;
+
+			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+
+			return;
+		}
+
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+		$class_names = "columns ".$class_names;
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $class_names .'>';
+
+		$atts = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		/** This filter is documented in wp-includes/post-template.php */
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
+	function end_el(&$output, $item, $depth=0, $args=array()) {
+		if( 0 == $depth ){
+			$output .= '</ul>';
+			return;
+		}
+		parent::end_el($output, $item, $depth, $args);
+	}
+}
