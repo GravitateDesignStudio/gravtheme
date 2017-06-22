22ef2df5e402cc36fb404ac33ab5e499ca4d8665
ggedde <geoffgedde@gmail.com>
2017-06-22 01:50:16 -0700

    Updated Variables and Forms

--- assets/js/master.js
+++ assets/js/master.js
@@ -11,16 +11,14 @@ jQuery(function($){
 	*
 	*/
 
-	function filter_links()
-	{
-		/* Make all External Links and PDF's open in a new Tab */
-	    var host = new RegExp('/' + window.location.host + '/');
-	    $('a').each(function() {
-		    if ((!host.test(this.href) && this.href.slice(0, 1) != "/" && this.href.slice(0, 1) != "#" && this.href.slice(0, 1) != "?") || this.href.indexOf('.pdf') > 0) {
-			    $(this).attr({'target': '_blank'});
-		    }
-		});
-	}
+
+
+
+
+
+
+
+
 
 	/*
 	* End Functions
@@ -35,7 +33,12 @@ jQuery(function($){
 	*/
 	$(document).ready(function(){
 
-		filter_links();
+		grav.filterLinks();
+		grav.setHeightVars();
+		grav.setScrollVars();
+		grav.addDropDownsToSubMenus();
+		grav.addListenerToMobileMenuButton();
+		grav.wrapInputItems();
 
 	});
 
