746a7c2203a8a1b8ec652e7f69c602493d63176e
Doug Frei <doug.frei@gravitatedesign.com>
2018-02-07 12:18:01 -0800

    UPDATE: added 'local_config.json' support for gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,19 @@
+function loadLocalConfig() {
+	const fs = require('fs');
+
+	try {
+		return JSON.parse(fs.readFileSync('local_config.json'));
+	} catch (e) {
+		return false;
+	}
+}
+
 const gulp			= require('gulp');
 const plugins		= require('gulp-load-plugins')();
 const browsersync	= require('browser-sync').create();
 const webpack		= require('webpack');
 const webpackStream	= require('webpack-stream');
 const webpackConfig	= require('./webpack.config.js');
-
-// gulp.task('scss', function () {
-// 	return gulp.src('css/master.scss')
-// 		.pipe(plugins.sourcemaps.init())
-// 		.pipe(plugins.sass())
-// 		.on('error', plugins.sass.logError)
-// 		.pipe(plugins.autoprefixer({
-// 			browsers: ['last 2 versions', 'ie >= 11']
-// 		}))
-// 		.pipe(plugins.cleanCss({
-// 			keepSpecialComments: 0
-// 		}))
-// 		.pipe(plugins.rename('master.min.css'))
-// 		.pipe(plugins.sourcemaps.write('.'))
-// 		.pipe(gulp.dest('dist/css'));
-// });
-
-// // gulp.task('scss-editor-styles', function () {
-// // 	return gulp.src('assets/css/editor-styles.scss');
-// // });
-
-// gulp.task('js', function () {
-// 	return gulp.src('js/master.js')
-// 		.pipe(webpackStream(webpackConfig, webpack))
-// 		.pipe(gulp.dest('dist/js'));
-// });
-
-// gulp.task('browser-sync', function () {
-// 	browsersync.init({
-// 		proxy: 'gravtheme.local.com',
-// 		files: [
-// 			'dist/css/master.min.css',
-// 			'dist/js/master.min.js',
-// 			'**/*.php'
-// 		],
-// 		open: false,
-// 		notify: true,
-// 		https: false
-// 	});
-// });
-
-// gulp.task('default', ['scss', 'js', 'browser-sync'], function () {
-// 	gulp.watch('css/**/*.scss', ['scss']);
-// 	gulp.watch('js/**/*.js', ['js']);
-// });
 
 gulp.task('scss', function () {
 	return gulp.src('css/master.scss')
@@ -64,20 +29,47 @@ gulp.task('scss', function () {
 		.pipe(plugins.rename('master.min.css'))
 		.pipe(plugins.sourcemaps.write('.'))
 		.pipe(gulp.dest('dist/css'))
-		.pipe(browsersync.stream());
+		.pipe(browsersync.stream())
+		.on('error', plugins.notify.onError({
+			message: '<%= error.message %>',
+			title: 'SCSS Error'
+		}));
 });
 
-// gulp.task('scss-watch', ['scss'], function (done) {
-// 	browsersync.reload();
-// 	done();
-// });
-
-// gulp.task('scss-editor-styles', function () {
-// 	return gulp.src('assets/css/editor-styles.scss');
-// });
+gulp.task('scss-editor-styles', function () {
+	return gulp.src('css/editor-styles.scss')
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass())
+		.on('error', plugins.sass.logError)
+		.pipe(plugins.autoprefixer({
+			browsers: ['last 2 versions', 'ie >= 11']
+		}))
+		.pipe(plugins.cleanCss({
+			keepSpecialComments: 0
+		}))
+		.pipe(plugins.rename('editor-styles.min.css'))
+		.pipe(plugins.sourcemaps.write('.'))
+		.pipe(gulp.dest('dist/css'))
+		.on('error', plugins.notify.onError({
+			message: '<%= error.message %>',
+			title: 'SCSS (editor styles) Error'
+		}));
+});
 
 gulp.task('js', function () {
 	return gulp.src('js/master.js')
+		.pipe(plugins.plumber({
+			errorHandler: function (err) {
+				plugins.notify.onError({
+					title: 'JS Build Error',
+					message: err.message
+				})(err);
+			}
+		}))
+		.pipe(plugins.notify({
+			message: 'Starting JS build',
+			title: 'JS Build'
+		}))
 		.pipe(webpackStream(webpackConfig, webpack))
 		.pipe(gulp.dest('dist/js'));
 });
@@ -88,16 +80,18 @@ gulp.task('js-watch', ['js'], function (done) {
 });
 
 gulp.task('browser-sync', function () {
+	const localConfig = loadLocalConfig();
+
+	if (!localConfig) {
+		console.error('Unable to load "local_config.json" -- please use "local_config_example.json" as a template');
+		process.exit();
+	}
+
 	browsersync.init({
-		proxy: 'gravtheme.local.com',
-		// files: [
-		// 	'dist/css/master.min.css',
-		// 	'dist/js/master.min.js',
-		// 	'**/*.php'
-		// ],
+		proxy: localConfig.browserSync.proxy,
 		open: false,
 		notify: true,
-		https: false
+		https: localConfig.browserSync.https
 	});
 
 	gulp.watch('css/**/*.scss', ['scss']);
@@ -106,7 +100,10 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('default', ['scss', 'js', 'browser-sync'], function () {
-	// gulp.watch('css/**/*.scss', ['scss-watch']);
-	// gulp.watch('js/**/*.js', ['js-watch']);
-	// gulp.watch('**/*.php').on('change', browsersync.reload);
+	const localConfig = loadLocalConfig();
+
+	if (!localConfig) {
+		console.error('Unable to load "local_config.json" -- please use "local_config_example.json" as a template');
+		process.exit();
+	}
 });
